90566da8423fefa14f001171ffbad6498708758c
veronikaKircsev <veronika.kircsev@students.fhv.at>
2024-04-14 15:51:08 +0200

    fix port

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 import prettyCssModules from 'vite-plugin-pretty-css-modules';
 
 export default defineConfig({
+  server: {
+    port: 5173
+  },
   plugins: [react(), prettyCssModules()]
 });
